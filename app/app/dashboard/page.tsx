a3234100afc8d664ddcd13d66c2430fa8e4c3801
2710brian <info@marketinggroupmalaga.com>
2026-05-23 22:56:16 +0200

    Update page.tsx

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -1,89 +1,44 @@
-"use client";
-
-export default function Home() {
-  const handleLogin = () => {
-    window.location.href = "/dashboard";
-  };
-
+export default function Dashboard() {
   return (
     <main
       style={{
         background: "#111",
         color: "white",
         minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        padding: "40px",
         fontFamily: "Arial",
       }}
     >
+      <h1
+        style={{
+          fontSize: "42px",
+          marginBottom: "30px",
+        }}
+      >
+        SeniorGuiden dashboard
+      </h1>
+
       <div
         style={{
           background: "#1c1c1c",
-          padding: "40px",
+          padding: "25px",
           borderRadius: "12px",
-          width: "350px",
-          boxShadow: "0 0 30px rgba(0,0,0,0.5)",
+          marginBottom: "20px",
         }}
       >
-        <h1
-          style={{
-            marginBottom: "30px",
-            fontSize: "42px",
-            fontWeight: "bold",
-          }}
-        >
-          SeniorGuiden login
-        </h1>
+        <h2>Aalborg kommune</h2>
+        <p>62% fyldt</p>
+      </div>
 
-        <input
-          type="email"
-          placeholder="Email"
-          style={{
-            width: "100%",
-            padding: "14px",
-            marginBottom: "15px",
-            borderRadius: "8px",
-            border: "none",
-            background: "#2b2b2b",
-            color: "white",
-            fontSize: "16px",
-            boxSizing: "border-box",
-          }}
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          style={{
-            width: "100%",
-            padding: "14px",
-            marginBottom: "20px",
-            borderRadius: "8px",
-            border: "none",
-            background: "#2b2b2b",
-            color: "white",
-            fontSize: "16px",
-            boxSizing: "border-box",
-          }}
-        />
-
-        <button
-          onClick={handleLogin}
-          style={{
-            width: "100%",
-            padding: "14px",
-            borderRadius: "8px",
-            border: "none",
-            background: "white",
-            color: "#111",
-            fontWeight: "bold",
-            cursor: "pointer",
-            fontSize: "16px",
-          }}
-        >
-          Login
-        </button>
+      <div
+        style={{
+          background: "#1c1c1c",
+          padding: "25px",
+          borderRadius: "12px",
+        }}
+      >
+        <h2>Roskilde kommune</h2>
+        <p>48% fyldt</p>
       </div>
     </main>
   );
