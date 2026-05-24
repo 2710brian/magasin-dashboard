05ad54c746458451a666774f624f04b919a0408d
2710brian <info@marketinggroupmalaga.com>
2026-05-24 03:23:02 +0200

    Create PageEditor.tsx

---
+++ components/components/PageEditor.tsx
@@ -0,0 +1,162 @@
+type PageEditorProps = {
+  selectedPage: number;
+
+  setSelectedPage: (
+    page: number | null
+  ) => void;
+};
+
+export default function PageEditor({
+  selectedPage,
+  setSelectedPage,
+}: PageEditorProps) {
+  return (
+    <div>
+      {/* TOP */}
+
+      <div
+        style={{
+          display: "flex",
+
+          justifyContent:
+            "space-between",
+
+          alignItems: "center",
+
+          marginBottom: "30px",
+        }}
+      >
+        <div>
+          <h1>
+            Side {selectedPage}
+          </h1>
+
+          <p
+            style={{
+              color: "#888",
+            }}
+          >
+            Side editor
+          </p>
+        </div>
+
+        <button
+          onClick={() =>
+            setSelectedPage(
+              null
+            )
+          }
+          style={{
+            background:
+              "#1f1f1f",
+
+            border:
+              "1px solid #333",
+
+            color: "white",
+
+            padding:
+              "12px 18px",
+
+            borderRadius:
+              "10px",
+
+            cursor: "pointer",
+          }}
+        >
+          Tilbage til sider
+        </button>
+      </div>
+
+      {/* SIDE */}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent:
+            "center",
+        }}
+      >
+        <div
+          style={{
+            width: "500px",
+
+            aspectRatio:
+              "210 / 297",
+
+            background:
+              "#1b1b1b",
+
+            borderRadius:
+              "14px",
+
+            padding: "20px",
+
+            display: "grid",
+
+            gridTemplateColumns:
+              "1fr 1fr",
+
+            gap: "12px",
+          }}
+        >
+          <div
+            style={{
+              background:
+                "#22c55e",
+
+              borderRadius:
+                "10px",
+
+              padding: "15px",
+            }}
+          >
+            Hansen VVS
+          </div>
+
+          <div
+            style={{
+              background:
+                "#444",
+
+              borderRadius:
+                "10px",
+
+              padding: "15px",
+            }}
+          >
+            LEDIG
+          </div>
+
+          <div
+            style={{
+              background:
+                "#eab308",
+
+              borderRadius:
+                "10px",
+
+              padding: "15px",
+            }}
+          >
+            Reserveret
+          </div>
+
+          <div
+            style={{
+              background:
+                "#22c55e",
+
+              borderRadius:
+                "10px",
+
+              padding: "15px",
+            }}
+          >
+            Café Nytorv
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
