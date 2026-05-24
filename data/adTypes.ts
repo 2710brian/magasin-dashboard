3ca234df0b71fa9a643bb4603adb84944516cf24
2710brian <info@marketinggroupmalaga.com>
2026-05-24 04:03:30 +0200

    Create adTypes.ts

---
+++ data/adTypes.ts
@@ -0,0 +1,83 @@
+export const adTypes = [
+  {
+    id: "helside",
+
+    name: "Helside",
+
+    width: 1,
+
+    height: 1,
+
+    orientation:
+      "portrait",
+  },
+
+  {
+    id: "half-horizontal",
+
+    name:
+      "1/2 vandret",
+
+    width: 1,
+
+    height: 0.5,
+
+    orientation:
+      "landscape",
+  },
+
+  {
+    id: "half-vertical",
+
+    name:
+      "1/2 lodret",
+
+    width: 0.5,
+
+    height: 1,
+
+    orientation:
+      "portrait",
+  },
+
+  {
+    id: "quarter",
+
+    name: "1/4 side",
+
+    width: 0.5,
+
+    height: 0.5,
+
+    orientation:
+      "square",
+  },
+
+  {
+    id: "banner-top",
+
+    name:
+      "Top banner",
+
+    width: 1,
+
+    height: 0.25,
+
+    orientation:
+      "landscape",
+  },
+
+  {
+    id: "double",
+
+    name:
+      "Dobbeltside",
+
+    width: 2,
+
+    height: 1,
+
+    orientation:
+      "landscape",
+  },
+];
