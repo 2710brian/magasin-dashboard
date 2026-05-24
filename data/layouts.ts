41859568d602f9c30aa4ce0cfd0eef4d9c34f672
2710brian <info@marketinggroupmalaga.com>
2026-05-24 03:40:58 +0200

    Create layouts.ts

---
+++ data/layouts.ts
@@ -0,0 +1,25 @@
+export const layouts = [
+  {
+    id: "helside",
+
+    name: "1 helside",
+
+    type: "single",
+  },
+
+  {
+    id: "halv-horizontal",
+
+    name: "2 halve",
+
+    type: "double",
+  },
+
+  {
+    id: "4-kvart",
+
+    name: "4 kvart",
+
+    type: "grid",
+  },
+];
