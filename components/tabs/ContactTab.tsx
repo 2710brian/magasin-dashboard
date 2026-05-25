970473fd5340540e2078bf01577ebbe96639d1c8
2710brian <info@marketinggroupmalaga.com>
2026-05-25 14:53:11 +0200

    Update ContactTab.tsx

--- components/tabs/ContactTab.tsx
+++ components/tabs/ContactTab.tsx
@@ -1,9 +1,12 @@
 type ContactTabProps = {
   ad: any;
+
+  setAd: any;
 };
 
 export default function ContactTab({
   ad,
+  setAd,
 }: ContactTabProps) {
   return (
     <div
@@ -18,63 +21,244 @@ export default function ContactTab({
     >
       <Field label="Virksomhed">
         <input
-          defaultValue={
-            ad.title
+          value={
+            ad.title || ""
           }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                title:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="CIF / VAT">
         <input
+          value={
+            ad.vat || ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                vat: e
+                  .target
+                  .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Kontaktperson">
         <input
+          value={
+            ad.contactPerson ||
+            ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                contactPerson:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Email">
         <input
+          value={
+            ad.email || ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                email:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Telefon">
         <input
+          value={
+            ad.phone || ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                phone:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Website">
         <input
+          value={
+            ad.website || ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                website:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Facebook">
         <input
+          value={
+            ad.facebook ||
+            ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                facebook:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="Instagram">
         <input
+          value={
+            ad.instagram ||
+            ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                instagram:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="LinkedIn">
         <input
+          value={
+            ad.linkedin ||
+            ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                linkedin:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
 
       <Field label="TikTok">
         <input
+          value={
+            ad.tiktok || ""
+          }
+
+          onChange={(e) =>
+            setAd(
+              (
+                prev: any
+              ) => ({
+                ...prev,
+
+                tiktok:
+                  e.target
+                    .value,
+              })
+            )
+          }
+
           style={inputStyle}
         />
       </Field>
