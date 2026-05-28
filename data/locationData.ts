359a9d32e937940c6cf14b0f7ace0559e6813d3d
2710brian <info@marketinggroupmalaga.com>
2026-05-28 14:06:01 +0200

    Update locationData.ts

--- data/locationData.ts
+++ data/locationData.ts
@@ -1,1 +1,141 @@
+export const locationData = {
 
+  Danmark: {
+
+    "Region Hovedstaden": {
+      kommuner: [
+        "København",
+        "Frederiksberg",
+        "Helsingør",
+        "Hillerød",
+        "Lyngby-Taarbæk",
+      ],
+    },
+
+    "Region Sjælland": {
+      kommuner: [
+        "Roskilde",
+        "Næstved",
+        "Slagelse",
+        "Køge",
+        "Holbæk",
+      ],
+    },
+
+    "Region Syddanmark": {
+      kommuner: [
+        "Odense",
+        "Esbjerg",
+        "Kolding",
+        "Vejle",
+        "Sønderborg",
+      ],
+    },
+
+    "Region Midtjylland": {
+      kommuner: [
+        "Aarhus",
+        "Randers",
+        "Herning",
+        "Silkeborg",
+        "Viborg",
+      ],
+    },
+
+    "Region Nordjylland": {
+      kommuner: [
+        "Aalborg",
+        "Hjørring",
+        "Brønderslev",
+        "Jammerbugt",
+        "Frederikshavn",
+      ],
+    },
+  },
+
+  Spanien: {
+
+    Andalucía: {
+
+      Málaga: [
+        "Málaga",
+        "Marbella",
+        "Fuengirola",
+        "Mijas",
+        "Benalmádena",
+        "Estepona",
+        "Torremolinos",
+        "Nerja",
+      ],
+
+      Cádiz: [
+        "Cádiz",
+        "Tarifa",
+        "Algeciras",
+      ],
+
+      Sevilla: [
+        "Sevilla",
+      ],
+
+      Granada: [
+        "Granada",
+      ],
+    },
+
+    "Comunidad Valenciana": {
+
+      Alicante: [
+        "Alicante",
+        "Benidorm",
+        "Torrevieja",
+      ],
+
+      Valencia: [
+        "Valencia",
+      ],
+    },
+
+    Cataluña: {
+
+      Barcelona: [
+        "Barcelona",
+      ],
+
+      Girona: [
+        "Girona",
+      ],
+    },
+
+    Madrid: {
+
+      Madrid: [
+        "Madrid",
+      ],
+    },
+
+    Canarias: {
+
+      Tenerife: [
+        "Santa Cruz",
+        "Costa Adeje",
+      ],
+
+      "Las Palmas": [
+        "Las Palmas",
+      ],
+    },
+
+    Baleares: {
+
+      Mallorca: [
+        "Palma",
+        "Magaluf",
+      ],
+
+      Ibiza: [
+        "Ibiza",
+      ],
+    },
+  },
+};
